ba7283373c6dca5078c160459410df6353ea1841
Václav Schubert <162372188+VaclavSchubert@users.noreply.github.com>
2026-04-29 16:13:57 +0200

    doc: remove template integration name

--- custom_components/photovoltaic_manager/config_flow.py
+++ custom_components/photovoltaic_manager/config_flow.py
@@ -187,7 +187,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
 
 class ManagerConfigFlow(ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Example Integration."""
+    """Handle a config flow for Photovoltaic Manager Integration."""
 
     VERSION = 1
     _input_data: dict[str, Any]
